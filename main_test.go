21598d911ba5ca04372f61bafef4416cbee049c8
Codeweavers Infrastructure <36475663+infraweavers@users.noreply.github.com>
2021-10-18 17:25:45 +0100

    Laying out the args a little clearer

--- main_test.go
+++ main_test.go
@@ -20,7 +20,14 @@ func TestArgumentParsing(t *testing.T) {
 
 		flag.CommandLine = flag.NewFlagSet("flag", flag.ExitOnError)
 
-		os.Args = []string{"main.exe", "-host", "remotehost", "-username", "thisismyusername", "-password", "thisismypassword", "-executable", "/path/to/executable", "-script", "README.md"}
+		os.Args = []string{
+			"main.exe",
+			"-host", "remotehost",
+			"-username", "thisismyusername",
+			"-password", "thisismypassword",
+			"-executable", "/path/to/executable",
+			"-script", "README.md",
+		}
 		httpClient := httpclient.NewMockHTTPClient(`{"output": "Test output", "exitcode": 2}`, 200)
 
 		var buf bytes.Buffer
@@ -43,26 +50,15 @@ func TestArgumentParsing(t *testing.T) {
 		oldArgs := os.Args
 		defer func() { os.Args = oldArgs }()
 		flag.CommandLine = flag.NewFlagSet("flag", flag.ExitOnError)
-		os.Args = []string{"main.exe", "-host", "remotehost", "-username", "thisismyusername", "-password", "thisismypassword", "-executable", "/path/to/executable", "-script", "README.md", "-insecure"}
-		httpClient := httpclient.NewMockHTTPClient(`{"output": "Test output", "exitcode": 2}`, 200)
-		var buf bytes.Buffer
-		actualExit := invokeClient(&buf, httpClient)
-		actualOutput := buf.String()
-		assert.Equal(t, `{"args":null,"path":"/path/to/executable","scriptarguments":[],"stdin":"# monitoring-agent-client","timeout":"10s"}`, httpClient.RequestBodyContent)
-		assert.Equal(t, 10*time.Second, httpClient.Timeout)
-		assert.Equal(t, true, httpClient.Transport.TLSClientConfig.InsecureSkipVerify)
-		assert.Equal(t, "Basic dGhpc2lzbXl1c2VybmFtZTp0aGlzaXNteXBhc3N3b3Jk", httpClient.RequestHeaders["Authorization"][0])
-		assert.Equal(t, "remotehost:9000", httpClient.RequestHost)
-		assert.Equal(t, "/v1/runscriptstdin", httpClient.RequestURI.Path)
-		assert.Equal(t, "POST", httpClient.RequestVerb)
-		assert.Equal(t, 2, actualExit)
-		assert.Equal(t, "Test output", actualOutput)
-	})
-	t.Run("Insecure basic test returns 200 and renders correct exit code", func(t *testing.T) {
-		oldArgs := os.Args
-		defer func() { os.Args = oldArgs }()
-		flag.CommandLine = flag.NewFlagSet("flag", flag.ExitOnError)
-		os.Args = []string{"main.exe", "-host", "remotehost", "-username", "thisismyusername", "-password", "thisismypassword", "-executable", "/path/to/executable", "-script", "README.md", "-insecure"}
+		os.Args = []string{
+			"main.exe",
+			"-host", "remotehost",
+			"-username", "thisismyusername",
+			"-password", "thisismypassword",
+			"-executable", "/path/to/executable",
+			"-script", "README.md",
+			"-insecure",
+		}
 		httpClient := httpclient.NewMockHTTPClient(`{"output": "Test output", "exitcode": 2}`, 200)
 		var buf bytes.Buffer
 		actualExit := invokeClient(&buf, httpClient)
@@ -81,7 +77,16 @@ func TestArgumentParsing(t *testing.T) {
 		oldArgs := os.Args
 		defer func() { os.Args = oldArgs }()
 		flag.CommandLine = flag.NewFlagSet("flag", flag.ExitOnError)
-		os.Args = []string{"main.exe", "-host", "remotehost", "-username", "thisismyusername", "-password", "thisismypassword", "-executable", "/path/to/executable", "-script", "README.md", "-executableArg", "arg1", "-executableArg", "arg2"}
+		os.Args = []string{
+			"main.exe",
+			"-host", "remotehost",
+			"-username", "thisismyusername",
+			"-password", "thisismypassword",
+			"-executable", "/path/to/executable",
+			"-script", "README.md",
+			"-executableArg", "arg1",
+			"-executableArg", "arg2",
+		}
 		httpClient := httpclient.NewMockHTTPClient(`{"output": "Test output", "exitcode": 2}`, 200)
 		var buf bytes.Buffer
 		actualExit := invokeClient(&buf, httpClient)
@@ -100,8 +105,17 @@ func TestArgumentParsing(t *testing.T) {
 		oldArgs := os.Args
 		defer func() { os.Args = oldArgs }()
 		flag.CommandLine = flag.NewFlagSet("flag", flag.ExitOnError)
-		os.Args = []string{"main.exe", "-host", "remotehost", "-username", "thisismyusername", "-password", "thisismypassword", "-executable", "/path/to/executable", "-script", "README.md",
-			"-executableArg", "arg1", "-executableArg", "arg2", "--", "scriptarg1", "-scriptarg scriptarg2", "-scriptarg", "scriptarg3", "--warning=3"}
+		os.Args = []string{
+			"main.exe", 
+			"-host", "remotehost", 
+			"-username", "thisismyusername", 
+			"-password", "thisismypassword", 
+			"-executable", "/path/to/executable", 
+			"-script", "README.md",
+			"-executableArg", "arg1", 
+			"-executableArg", "arg2", 
+			"--", "scriptarg1", "-scriptarg scriptarg2", "-scriptarg", "scriptarg3", "--warning=3"
+		}
 		httpClient := httpclient.NewMockHTTPClient(`{"output": "Test output", "exitcode": 2}`, 200)
 		var buf bytes.Buffer
 		actualExit := invokeClient(&buf, httpClient)
