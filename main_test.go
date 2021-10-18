9f4e50693dfaca834337fc776dc9a6e4562be35a
Codeweavers Infrastructure <36475663+infraweavers@users.noreply.github.com>
2021-10-18 17:16:01 +0100

    When provided, the client certificate should be loaded into the TLSClientConfig

--- main_test.go
+++ main_test.go
@@ -116,4 +116,35 @@ func TestArgumentParsing(t *testing.T) {
 		assert.Equal(t, 2, actualExit)
 		assert.Equal(t, "Test output", actualOutput)
 	})
+
+	t.Run("Test the client certificate and key are loaded corrected", func(t *testing.T) {
+		oldArgs := os.Args
+		defer func() { os.Args = oldArgs }()
+		flag.CommandLine = flag.NewFlagSet("flag", flag.ExitOnError)
+		os.Args = []string{"main.exe",
+			"-host", "remotehost",
+			"-username", "thisismyusername",
+			"-password", "thisismypassword",
+			"-executable", "/path/to/executable",
+			"-certificate", "server.crt",
+			"-key", "server.key",
+			"-script", "README.md",
+		}
+		httpClient := httpclient.NewMockHTTPClient(`{"output": "Test output", "exitcode": 1}`, 200)
+		var buf bytes.Buffer
+		actualExit := invokeClient(&buf, httpClient)
+		actualOutput := buf.String()
+		assert.Equal(t, `{"args":null,"path":"/path/to/executable","scriptarguments":[],"stdin":"# monitoring-agent-client","timeout":"10s"}`, httpClient.RequestBodyContent)
+		assert.Equal(t, 10*time.Second, httpClient.Timeout)
+		assert.Equal(t, false, httpClient.Transport.TLSClientConfig.InsecureSkipVerify)
+		assert.Equal(t, 1, len(httpClient.Transport.TLSClientConfig.Certificates))
+		assert.NotNil(t, httpClient.Transport.TLSClientConfig.Certificates[0].Certificate)
+		assert.NotNil(t, httpClient.Transport.TLSClientConfig.Certificates[0].PrivateKey)
+		assert.Equal(t, "Basic dGhpc2lzbXl1c2VybmFtZTp0aGlzaXNteXBhc3N3b3Jk", httpClient.RequestHeaders["Authorization"][0])
+		assert.Equal(t, "remotehost:9000", httpClient.RequestHost)
+		assert.Equal(t, "/v1/runscriptstdin", httpClient.RequestURI.Path)
+		assert.Equal(t, "POST", httpClient.RequestVerb)
+		assert.Equal(t, 1, actualExit)
+		assert.Equal(t, "Test output", actualOutput)
+	})
 }
