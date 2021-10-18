e41e19237ca04e58d9473ad8fc390f74d4d3740f
Codeweavers Infrastructure <36475663+infraweavers@users.noreply.github.com>
2021-10-18 17:33:21 +0100

    Adding test that the cacert is loaded when provided

--- main_test.go
+++ main_test.go
@@ -106,15 +106,15 @@ func TestArgumentParsing(t *testing.T) {
 		defer func() { os.Args = oldArgs }()
 		flag.CommandLine = flag.NewFlagSet("flag", flag.ExitOnError)
 		os.Args = []string{
-			"main.exe", 
-			"-host", "remotehost", 
-			"-username", "thisismyusername", 
-			"-password", "thisismypassword", 
-			"-executable", "/path/to/executable", 
+			"main.exe",
+			"-host", "remotehost",
+			"-username", "thisismyusername",
+			"-password", "thisismypassword",
+			"-executable", "/path/to/executable",
 			"-script", "README.md",
-			"-executableArg", "arg1", 
-			"-executableArg", "arg2", 
-			"--", "scriptarg1", "-scriptarg scriptarg2", "-scriptarg", "scriptarg3", "--warning=3"
+			"-executableArg", "arg1",
+			"-executableArg", "arg2",
+			"--", "scriptarg1", "-scriptarg scriptarg2", "-scriptarg", "scriptarg3", "--warning=3",
 		}
 		httpClient := httpclient.NewMockHTTPClient(`{"output": "Test output", "exitcode": 2}`, 200)
 		var buf bytes.Buffer
@@ -154,6 +154,34 @@ func TestArgumentParsing(t *testing.T) {
 		assert.Equal(t, 1, len(httpClient.Transport.TLSClientConfig.Certificates))
 		assert.NotNil(t, httpClient.Transport.TLSClientConfig.Certificates[0].Certificate)
 		assert.NotNil(t, httpClient.Transport.TLSClientConfig.Certificates[0].PrivateKey)
+		assert.Equal(t, "Basic dGhpc2lzbXl1c2VybmFtZTp0aGlzaXNteXBhc3N3b3Jk", httpClient.RequestHeaders["Authorization"][0])
+		assert.Equal(t, "remotehost:9000", httpClient.RequestHost)
+		assert.Equal(t, "/v1/runscriptstdin", httpClient.RequestURI.Path)
+		assert.Equal(t, "POST", httpClient.RequestVerb)
+		assert.Equal(t, 1, actualExit)
+		assert.Equal(t, "Test output", actualOutput)
+	})
+
+	t.Run("Test the CACertificate is loaded correctly", func(t *testing.T) {
+		oldArgs := os.Args
+		defer func() { os.Args = oldArgs }()
+		flag.CommandLine = flag.NewFlagSet("flag", flag.ExitOnError)
+		os.Args = []string{"main.exe",
+			"-host", "remotehost",
+			"-username", "thisismyusername",
+			"-password", "thisismypassword",
+			"-executable", "/path/to/executable",
+			"-cacert", "cacert.pem",
+			"-script", "README.md",
+		}
+		httpClient := httpclient.NewMockHTTPClient(`{"output": "Test output", "exitcode": 1}`, 200)
+		var buf bytes.Buffer
+		actualExit := invokeClient(&buf, httpClient)
+		actualOutput := buf.String()
+		assert.Equal(t, `{"args":null,"path":"/path/to/executable","scriptarguments":[],"stdin":"# monitoring-agent-client","timeout":"10s"}`, httpClient.RequestBodyContent)
+		assert.Equal(t, 10*time.Second, httpClient.Timeout)
+		assert.Equal(t, false, httpClient.Transport.TLSClientConfig.InsecureSkipVerify)
+		assert.NotNil(t, httpClient.Transport.TLSClientConfig.RootCAs)
 		assert.Equal(t, "Basic dGhpc2lzbXl1c2VybmFtZTp0aGlzaXNteXBhc3N3b3Jk", httpClient.RequestHeaders["Authorization"][0])
 		assert.Equal(t, "remotehost:9000", httpClient.RequestHost)
 		assert.Equal(t, "/v1/runscriptstdin", httpClient.RequestURI.Path)
